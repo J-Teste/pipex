c43726d2673ff505114ee9efc24d22a48aca914d
Niagami <jason.tste@gmail.com>
2024-04-02 02:58:19 +0200

    ok

--- printf/libft/ft_strjoin.c
+++ printf/libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: jteste <jteste@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 09:46:51 by jteste            #+#    #+#             */
-/*   Updated: 2024/04/02 02:48:19 by jteste           ###   ########.fr       */
+/*   Updated: 2024/04/02 02:57:47 by jteste           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	i = 0;
 	j = 0;
     if (s1 == NULL || s2 == NULL)
-        return NULL;
-	len = (ft_strlen((char *)s1) + ft_strlen((char *)s2)) + 1;
+	{
+        return NULL;	
+	}
+	len = (ft_strlen(s1) + ft_strlen(s2)) + 1;
 	str = (char *)malloc(sizeof(char) * len);
 	if (!str)
 		return (0);
